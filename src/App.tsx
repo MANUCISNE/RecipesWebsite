e3e754022d823c4e0999b2a0a5f6c01bf62eb4ac
MANUCISNE <117774887+MANUCISNE@users.noreply.github.com>
2024-02-18 14:26:06 -0300

    project Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.scss";
-import RecipeSearch from "./components/RecipeSearch";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
@@ -18,15 +17,11 @@ function App() {
       <header>
         <div className="container">
           <div className="row">
-            <span>Boas vindas, [nome da pessoa]</span>
+            <span>Boas vindas,</span>
           </div>
         </div>
       </header>
       <div className="container">
-        <section>
-          <h2>Buscar Receitas</h2>
-          <RecipeSearch />
-        </section>
         <section>
           <h2>Lista de Receitas</h2>
           <RecipeList onRecipeSelect={handleRecipeSelect} />
--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import recipesData from "../data/recipesData.json";
+import React, { useState } from "react";
+import recipesData from '../data/recipesData.json';
 
 type Recipe = {
   id: number;
   title: string;
   category: string;
   image: string;
+  ingredients: string[];
+  instructions: string[];
+  prepTime: string;
+  servings: string;
+  vegetarian: boolean;
+  glutenFree: boolean;
+  videoUrl: string | null;
+};
+
+type RecipesData = {
+  recipes: Recipe[];
 };
 
 type RecipeListProps = {
@@ -13,20 +24,56 @@ type RecipeListProps = {
 };
 
 const RecipeList: React.FC<RecipeListProps> = ({ onRecipeSelect }) => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<Recipe[]>([]);
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    const results = (recipesData as RecipesData).recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    if (value === '') {
+      setSearchResults([]);
+    }
+  };
+
   return (
-    <div className="recipe-card-container">
-      {recipesData.recipes.map((recipe: Recipe) => (
-        <div
-          key={recipe.id}
-          className="recipe-card"
-          onClick={() => onRecipeSelect(recipe.id)}
-        >
-          <h2>{recipe.title}</h2>
-          <img src={recipe.image} alt={recipe.title} />
-          <p>{recipe.category}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="RecipeSearch">
+        <form className="RecipeSearch-search-form" onSubmit={handleSearch}>
+          <input
+            className="RecipeSearch-search-input"
+            type="text"
+            placeholder="Buscar por título"
+            value={searchTerm}
+            onChange={handleInputChange}
+          />
+          <button className="RecipeSearch-search-button" type="submit">
+            Buscar
+          </button>
+        </form>
+      </div>
+      
+      <div className="recipe-card-container">
+        {(searchResults.length === 0 ? recipesData.recipes : searchResults).map((recipe: Recipe) => (
+          <div
+            key={recipe.id}
+            className="recipe-card"
+            onClick={() => onRecipeSelect(recipe.id)}
+          >
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image} alt={recipe.title} />
+            <p>{recipe.category}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
